b6a54e511d32be91d6bd079884419b7fc81e162b
hibana2077 <hibana2077@gmail.com>
2025-10-08 11:39:58 +0800

    fix: Ensure HF_HUB_OFFLINE is set in T001.sh for consistent environment configuration

--- script/T/T001.sh
+++ script/T/T001.sh
@@ -11,6 +11,7 @@
 module load cuda/12.6.2
 source /scratch/rp06/sl5952/VC-SA/.venv/bin/activate
 export HF_HOME="/scratch/rp06/sl5952/VC-SA/.cache"
+export HF_HUB_OFFLINE=1
 
 cd ../..
 python3 -m src.run --dataset hmdb51 --data-root ./datasets/hmdb51 --frames-per-clip 16 --batch-size 8 --num-workers 12 >> T001.log 2>&1
