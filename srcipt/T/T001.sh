14675b0f283c0df8d47f94be2c902a9944693e5a
hibana2077 <hibana2077@gmail.com>
2025-10-08 11:23:03 +0800

    feat: Add initial job script for HMDB51 dataset processing with PBS configuration

---
+++ srcipt/T/T001.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=24GB
+#PBS -l walltime=01:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+source /scratch/rp06/sl5952/VC-SA/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/VC-SA/.cache"
+
+cd ../..
+python3 -m src.run --dataset hmdb51 --data-root ./datasets/hmdb51 --frames-per-clip 16 --batch-size 8 --num-workers 12 >> T001.log 2>&1
