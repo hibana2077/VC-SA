41746bb72614924201378e292a371dee09ce2ffb
hibana2077 <hibana2077@gmail.com>
2025-10-09 18:01:35 +0800

    feat: Add multiple model scripts and update M_score.md with new models and test accuracies

---
+++ script/MM/M001.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=16GB
+#PBS -l walltime=10:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+#PBS -l jobfs=2GB
+
+module load cuda/12.6.2
+module load ffmpeg/4.1.3
+source /scratch/rp06/sl5952/VC-SA/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/VC-SA/.cache"
+export HF_HUB_OFFLINE=1
+# cait_s24_224.fb_dist_in1k
+cd ../..
+python3 -m src.run --dataset hmdb51 --data-root ./datasets/hmdb51 --freeze-backbone --vit-name cait_s24_224.fb_dist_in1k --frames-per-clip 12 --square-num-dirs 10 --batch-size 2 --num-workers 4 --precision 32 --no-future-warning --no-user-warning --use-test-as-val --no-tqdm --print-interval 1000 >> TC001.log 2>&1
