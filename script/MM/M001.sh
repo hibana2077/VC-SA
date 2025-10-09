35f2f0659f3ac96cad89d7d25a978d3a248667ea
hibana2077 <hibana2077@gmail.com>
2025-10-09 18:09:54 +0800

    feat: Update M001.sh to log output to M001.log and add TC_score.txt for TC001 results

--- script/MM/M001.sh
+++ script/MM/M001.sh
@@ -16,4 +16,4 @@ export HF_HOME="/scratch/rp06/sl5952/VC-SA/.cache"
 export HF_HUB_OFFLINE=1
 # cait_s24_224.fb_dist_in1k
 cd ../..
-python3 -m src.run --dataset hmdb51 --data-root ./datasets/hmdb51 --freeze-backbone --vit-name cait_s24_224.fb_dist_in1k --frames-per-clip 12 --square-num-dirs 10 --batch-size 2 --num-workers 4 --precision 32 --no-future-warning --no-user-warning --use-test-as-val --no-tqdm --print-interval 1000 >> TC001.log 2>&1
+python3 -m src.run --dataset hmdb51 --data-root ./datasets/hmdb51 --freeze-backbone --vit-name cait_s24_224.fb_dist_in1k --frames-per-clip 12 --square-num-dirs 10 --batch-size 2 --num-workers 4 --precision 32 --no-future-warning --no-user-warning --use-test-as-val --no-tqdm --print-interval 1000 >> M001.log 2>&1
