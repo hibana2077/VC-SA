e053a46179659cffc8e32d47e3233a0586a182b5
hibana2077 <hibana2077@gmail.com>
2025-10-12 22:07:26 +0800

    feat: Add new ST scripts and update scoring documentation

--- script/ST/ST004.sh
+++ script/ST/ST004.sh
@@ -7,7 +7,7 @@
 #PBS -l walltime=10:00:00
 #PBS -l wd
 #PBS -l storage=scratch/rp06
-#PBS -l jobfs=4GB
+#PBS -l jobfs=2GB
 
 module load cuda/12.6.2
 module load ffmpeg/4.1.3
@@ -16,4 +16,4 @@ export HF_HOME="/scratch/rp06/sl5952/VC-SA/.cache"
 export HF_HUB_OFFLINE=1
 # deit3_base_patch16_224.fb_in22k_ft_in1k
 cd ../..
-python3 -m src.run --dataset hmdb51 --data-root ./datasets/hmdb51 --freeze-backbone --vit-name deit3_base_patch16_224.fb_in22k_ft_in1k --frames-per-clip 16 --frieren-num-dirs 10 --batch-size 2 --num-workers 4 --precision 32 --no-future-warning --no-user-warning --use-test-as-val --no-tqdm --print-interval 1000 >> RP001.log 2>&1
+python3 -m src.run --dataset hmdb51 --data-root ./datasets/hmdb51 --freeze-backbone --vit-name deit3_base_patch16_224.fb_in22k_ft_in1k --frames-per-clip 16 --frieren-num-dirs 6 --batch-size 2 --num-workers 4 --precision 32 --no-future-warning --no-user-warning --use-test-as-val --no-tqdm --print-interval 1000 >> ST004.log 2>&1
---
+++ script/ST/ST_score.md
@@ -0,0 +1,14 @@
+# M_score
+
+## Backbone Model
+
+- deit3_base_patch16_224.fb_in22k_ft_in1k
+
+## Summary of Test Accuracies
+
+| Code | Frames | num_dirs | Test Acc |
+|------|--------|----------|----------|
+| ST001 | 16 | 10 | ? |
+| ST002 | 16 | 12 | ? |
+| ST003 | 16 | 8 | ? |
+| ST004 | 16 | 6 | ? |
