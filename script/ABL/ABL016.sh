fc5bc522ff3134d5683d87355504b21d8bbf9e04
hibana2077 <hibana2077@gmail.com>
2025-10-13 21:20:18 +0800

    feat: Add ABL007.sh to ABL016.sh scripts for model evaluation on hmdb51 dataset with various models

---
+++ script/ABL/ABL016.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q dgxa100
+#PBS -l ngpus=1
+#PBS -l ncpus=16
+#PBS -l mem=16GB
+#PBS -l walltime=10:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+#PBS -l jobfs=2GB
+
+module load cuda/12.6.2
+module load ffmpeg/4.1.3
+source /scratch/rp06/sl5952/VC-SA/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/VC-SA/.cache"
+export HF_HUB_OFFLINE=1
+# tnt_b_patch16_224.in1k
+cd ../..
+python3 -m src.run --dataset hmdb51 --data-root ./datasets/hmdb51 --freeze-backbone --vit-name tnt_b_patch16_224.in1k --frames-per-clip 12 --frieren-num-dirs 10 --batch-size 2 --num-workers 4 --precision 32 --no-future-warning --no-user-warning --use-test-as-val --no-tqdm --wo-square --print-interval 1000 >> ABL016.log 2>&1
